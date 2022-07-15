f8a1bfbdfcc5484ed33ade89d1e1d805d490fed1
Alexander Schranz <alexander@sulu.io>
2022-07-15 12:24:43 +0200

    Add missing test case
     Conflicts:
    	composer.lock

---
+++ tests/Model/AddRemove/Fixture/model_with_add_remove_datetime.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App;
+
+class ModelWithAddRemoveDateTime
+{
+    /**
+     * @var iterable<\DateTime>
+     */
+    private iterable $datetimes = [];
+
+    public function getDateTimes(): iterable
+    {
+        return $this->datetimes;
+    }
+
+    public function addDateTime(\DateTime $datetime): void
+    {
+        $this->datetimes[] = $datetime;
+    }
+
+    public function removeDateTime(\DateTime $datetime): void
+    {
+        foreach ($this->datetimes as $key => $currentDatetime) {
+            if ($datetime === $currentDatetime) {
+                unset($this->datetimes[$key]);
+            }
+        }
+    }
+}
+
+?>
+---
+<?php
+
+namespace App\Tests\Unit;
+
+use App\ModelWithAddRemoveDateTime;
+use DateTime;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \App\ModelWithAddRemoveDateTime
+ */
+class ModelWithAddRemoveDateTimeTest extends TestCase
+{
+    public function testGetDateTimes(): void
+    {
+        $model = $this->createInstance();
+        $this->assertSame('TODO', $model->getDateTimes());
+        $this->markAsIncomplete('This was generated with "schranz/test-generator" and should be adjusted.');
+    }
+
+    public function testAddRemoveDateTime(): void
+    {
+        $model = $this->createInstance();
+        $datetime = new DateTime('2022-01-01');
+        $model->addDateTime($datetime);
+        $this->assertContains($datetime, $model->getDateTimes());
+        $model->removeDateTime($datetime);
+        $this->assertNotContains($datetime, $model->getDateTimes());
+        $this->markAsIncomplete('This was generated with "schranz/test-generator" and should be adjusted.');
+    }
+
+    public function createInstance(): ModelWithAddRemoveDateTime
+    {
+        return new ModelWithAddRemoveDateTime();
+    }
+}
